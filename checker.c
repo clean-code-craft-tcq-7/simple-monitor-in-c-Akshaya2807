c8bfa88f8ec71905b595e48b864b212632f44398
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-12-10 18:39:34 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
 
-int CheckBatteryTemp(float temperature)
-{
-  if(temperature < 0 || temperature > 45) {
-  printf("Temperature out of range!\n");
-  return 0;}
+int CheckBatteryTemp(float temperature){
+ if(temperature < 0 || temperature > 45) {
+ printf("Temperature out of range!\n");
+ return 0;}
 return 1;
 }
 
-int CheckBatterysoc(float soc)
-{
-  if(soc < 20 || soc > 80) {
-  printf("State of Charge out of range!\n");
-  return 0;}
- return 1;
+int CheckBatterysoc(float soc){
+ if(soc < 20 || soc > 80) {
+ printf("State of Charge out of range!\n");
+ return 0;}
+return 1;
 }
 
 int CheckBatterychargeRate(float chargeRate){
-  if(chargeRate > 0.8) {
-  printf("Charge Rate out of range!\n");
-  return 0;}
- return 1;
+ if(chargeRate > 0.8) {
+ printf("Charge Rate out of range!\n");
+ return 0;}
+return 1;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if((CheckBatteryTemp(temperature) == 1) && (CheckBatterysoc(soc)== 1) && (CheckBatterychargeRate(chargeRate)==1))
-  {
-    return 1;}
-  else{
-   return 0;}
+ if((CheckBatteryTemp(temperature) == 1) && (CheckBatterysoc(soc)== 1) && (CheckBatterychargeRate(chargeRate)==1)){
+   return 1;}
+ else{
+  return 0;}
   }
 
 int main() {
